296a61c65d11a772eabf73ad2913ca70d450ac8e
Elizabeth <tjeudy.e@gmail.com>
2023-09-17 14:32:24 -0400

    home page structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,48 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from "react";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const Home = () => {
+	return (
+		<>
+			Hero-banner
+			<div>
+				<h2>Essential cruelty-free skincare for glowy and healthy skin</h2>
+				<button>Shop Our Products</button>
+			</div>
+			<div>
+				<p>OUR MISSION</p>
+				<h2>
+					We are on a mission to help you reduce plastic waste in your skincare
+					routine. It's simpler than it seems.
+				</h2>
+				{/* displays 4 product images*/}
+				{["Product 4", "Product 5", "Product 7", "Product 8"].map(
+					(product) => product
+				)}
+			</div>
+			<div>
+				<h2>What we stand for</h2>
+				<button>Shop Our Products</button>
+				{/* displays clean ingredients, vegan, sustainable, cruelty free*/}
+				{["Statement 1", "Statement 2", "Statement 3", "Statement 4"].map(
+					(statement) => statement
+				)}
+			</div>
+			<div>
+				<p>Everybody needs a skincare routine.</p>
+				<h2>
+					Take good care of your skin and hydrate. If you have good, glowing
+					skins, everything falls into place!
+				</h2>
+			</div>
+			<div>
+				<h2>Your skin has never looked this good!</h2>
+				<p>@essenceskincare</p>
+				<small>LEARN ABOUT SKINCARE PRODUCTS ON OUR INSTRAGRAM ACCOUNT</small>
+				{/* 4 FOOTER IMAGES */}
+			</div>
+			footer
+		</>
+	);
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
