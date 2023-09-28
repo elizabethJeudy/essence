009bbea9bdfdd459a5c9a4ad4b0b2fbd872e0ae1
Elizabeth <tjeudy.e@gmail.com>
2023-09-28 12:11:32 -0400

    product component renders products from sanity

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Link from "next/link";
+import { urlFor } from "../library/client";
 
-const Product = () => {
-	return <div>Product</div>;
+const Product = ({ product: { image, name, slug, price } }) => {
+	return (
+		<div>
+			<Link href={`/product/${slug.current}`} />
+			<div className="product-card">
+				<img
+					src={urlFor(image && image[0])}
+					width={250}
+					height={250}
+					className="product-image"
+				/>
+				<p className="product-name">{name}</p>
+				<p className="product-price">$ {price}.00 USD</p>
+			</div>
+		</div>
+	);
 };
 
 export default Product;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { use } from "react";
+import { useState } from "react";
 import { client } from "../library/client.js";
 import { HeroBanner, Product, FooterBanner } from "../components/index.js";
+import clean1 from "../public/assets/clean1.png";
 
 const Home = ({ products, bannerData }) => {
 	return (
@@ -15,19 +17,24 @@ const Home = ({ products, bannerData }) => {
 			</div>
 			<div className="products-container">
 				{/* displays 4 product images*/}
-				{products?.map((product) => product.name)}
+				{products?.slice(0, 4).map((product) => (
+					<Product key={product._id} product={product} />
+				))}
 			</div>
 			<div className="statements-container">
 				<h2>What we stand for</h2>
-
 				<button>Shop Our Products</button>
+				<img src={clean1} />
+
 				{/* displays clean ingredients, vegan, sustainable, cruelty free*/}
 			</div>
 			<div class="content-container">
 				<div class="row">
 					<div class="column">
 						<div class="content-box-one border-box">
-							<p>Everybody needs a skincare routine.</p>
+							<p className="box-one-tagline">
+								Everybody needs a skincare routine.
+							</p>
 							<h2>
 								Take good care of your skin and hydrate. If you have good,
 								glowing skins, everything falls into place!
@@ -35,14 +42,14 @@ const Home = ({ products, bannerData }) => {
 						</div>
 					</div>
 					<div class="column">
-						<div class="content-box-two border-box">EMPTY</div>
+						<div class="content-box-two border-box"></div>
 					</div>
 				</div>
 			</div>
 			<div class="content-container">
 				<div class="row">
 					<div class="column">
-						<div class="content-box-one">
+						<div class="content-box-one skincare">
 							<h2>Your skin has never looked this good!</h2>
 							<p>@essenceskincare</p>
 							<small>
@@ -55,6 +62,7 @@ const Home = ({ products, bannerData }) => {
 					</div>
 				</div>
 			</div>
+
 			<FooterBanner />
 		</>
 	);
@@ -73,3 +81,57 @@ export const getServerSideProps = async () => {
 };
 
 export default Home;
+
+/**
+ * 	const modal = document.getElementById("modal");
+	const modalCloseBtn = document.getElementById("modal-close-btn");
+	const subscribeForm = document.getElementById("subscribe-form");
+
+	setTimeout(function () {
+		modal.style.display = "inline";
+	}, 1500);
+
+	modalCloseBtn.addEventListener("click", function () {
+		modal.style.display = "none";
+	});
+
+	subscribeForm.addEventListener("submit", function (e) {
+		e.preventDefault();
+		setTimeout(function () {
+			document.getElementById("modal-inner").innerHTML = `
+<div className="modal-inner-text">
+	<p>Thank you for subscribing to our newsletter. We are thrilled to have you on board.</p>
+</div>
+			`;
+		});
+	});
+
+	return (
+		<div className="modal" id="modal">
+			<div className="close-modal-container">
+				<p>Subscribe</p>
+				<button className="modal-close-btn" id="modal-close-btn">
+					Close
+				</button>
+			</div>
+			<div className="modal-inner" id="modal-inner">
+				<h3>Save 10% off your order</h3>
+				<p className="modal-text" id="modal-text">
+					{" "}
+					Subscribe to our newsletter and get 10% off your first order.
+				</p>
+
+				<form className="subscribe-form">
+					<input
+						type="email"
+						name="email"
+						aria-label="Email address"
+						placeholder="Your email address"
+					></input>
+					<button type="submit">Subscribe</button>
+				</form>
+			</div>
+		</div>
+	)
+ * 
+ */
